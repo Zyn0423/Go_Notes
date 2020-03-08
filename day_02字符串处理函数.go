7d89435391b2c370f30658c0040aadd9278bb5c3
Zxn <zxnyn0423@163.com>
2020-03-08 20:01:13 +0800

    demo

--- day_02字符串处理函数.go
+++ day_02字符串处理函数.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
+func testSplit() {
+	str := "Now that we have a feel for what the data elements look like, how do we manage them with our application?"
+
+	ret := strings.Split(str, " ") //TODO 字符串按指定分割返回切片类型
+	fmt.Println(ret)
+	for _, s := range ret { //循环遍历的当时
+		fmt.Println(s)
+	}
+	ret2 := strings.Fields(str) // TODO 字符串按空格拆分
+	fmt.Println(ret2)
+
+}
+
+func hasfuffix_preffix() { // TODO 判断字符串结束标记和起始标记
+
+	result := strings.HasSuffix("Happybirthday.mp3", "mp3") //结束
+	fmt.Println(result)
+	result2 := strings.HasPrefix("Happybirthday.mp3", "Happybirthday") //起始
+	fmt.Println(result2)
+
+}
+
 func main() {
-	fmt.Println(strconv.AppendInt())
+	testSplit()
+	hasfuffix_preffix()
 }
