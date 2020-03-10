5e1ea4530d8391991d08fe2e94d64e2a443f792d
Zxn <zxnyn0423@163.com>
2020-03-10 17:35:30 +0800

    打开与创建

--- day_02文件打开与创建读写操作.go
+++ day_02文件打开与创建读写操作.go
@@ -20,21 +20,21 @@ func create_file() { //TODO 新建文件
 
 func open_file() { //TODO 打开文件并写入
 	/*
-	os.O_RDONLY: 以只读的方式打开
-	os.O_WRONLY: 以只写的方式打开
-	os.O_RDWR : 以读写的方式打开
-	os.O_NONBLOCK: 打开时不阻塞
-	os.O_APPEND: 以追加的方式打开
-	os.O_CREAT: 创建并打开一个新文件
-	os.O_TRUNC: 打开一个文件并截断它的长度为零（必须有写权限）
-	os.O_EXCL: 如果指定的文件存在，返回错误
-	os.O_SHLOCK: 自动获取共享锁
-	os.O_EXLOCK: 自动获取独立锁
-	os.O_DIRECT: 消除或减少缓存效果
-	os.O_FSYNC : 同步写入
-	os.O_NOFOLLOW: 不追踪软链接
+		os.O_RDONLY: 以只读的方式打开
+		os.O_WRONLY: 以只写的方式打开
+		os.O_RDWR : 以读写的方式打开
+		os.O_NONBLOCK: 打开时不阻塞
+		os.O_APPEND: 以追加的方式打开
+		os.O_CREAT: 创建并打开一个新文件
+		os.O_TRUNC: 打开一个文件并截断它的长度为零（必须有写权限）
+		os.O_EXCL: 如果指定的文件存在，返回错误
+		os.O_SHLOCK: 自动获取共享锁
+		os.O_EXLOCK: 自动获取独立锁
+		os.O_DIRECT: 消除或减少缓存效果
+		os.O_FSYNC : 同步写入
+		os.O_NOFOLLOW: 不追踪软链接
 	*/
-	f, err := os.OpenFile("./test.txt",os.O_RDWR, 6)
+	f, err := os.OpenFile("./test.txt", os.O_RDWR, 6)
 	if err != nil {
 		fmt.Println("打开失败", err)
 		return
@@ -50,8 +50,7 @@ func open_file() { //TODO 打开文件并写入
 
 }
 
-
 func main() {
-	//create_flie()
-	//open_file()
+	create_file()
+	open_file()
 }
