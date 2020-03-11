b5d878e3024e71738eb159e483f57f23c6691b08
Zxn <zxnyn0423@163.com>
2020-03-12 02:11:14 +0800

    文件目录操作

--- day_01/day_01值传递值引用.go
+++ day_01/day_01值传递值引用.go
@@ -5,58 +5,58 @@ import (
 	"strings"
 )
 
-func swap(a,b int)  {
-	a,b=b,a
-	fmt.Println("a=",a,"b=",b)
+func swap(a, b int) {
+	a, b = b, a
+	fmt.Println("a=", a, "b=", b)
 }
-func swap_1(a,b *int)  {
-	*a,*b=*b,*a   //*解引用
-	fmt.Println("a=",a,"b=",b)
+func swap_1(a, b *int) {
+	*a, *b = *b, *a //*解引用
+	fmt.Println("a=", a, "b=", b)
 }
 
-func slice_1()  {
+func slice_1() {
 	//练习1：给定一个字符串列表，在原有slice上返回不包含空字符串的列表， 如：
 	//	{"red", "", "black", "", "", "pink", "blue"}
 	//——> {"red", "black", "pink", "blue"}
 	/*
-	slice_ :=[]string{"red", "", "black", "", "", "pink", "blue"}
-	var slice_2 []string
-	for _,v :=range slice_{
-		if len(v) >0{
-			fmt.Println(v)
-			slice_2 = append(slice_2, v)
+		slice_ :=[]string{"red", "", "black", "", "", "pink", "blue"}
+		var slice_2 []string
+		for _,v :=range slice_{
+			if len(v) >0{
+				fmt.Println(v)
+				slice_2 = append(slice_2, v)
+			}
 		}
-	}
-	fmt.Println(slice_2)*/
+		fmt.Println(slice_2)*/
 	//方法二：
-	slice_ :=[]string{"red", "", "black", "", "", "pink", "blue"}
-	i:=0
-	for _,v :=range slice_{
-		if v != ""{
-			slice_[i]=v
+	slice_ := []string{"red", "", "black", "", "", "pink", "blue"}
+	i := 0
+	for _, v := range slice_ {
+		if v != "" {
+			slice_[i] = v
 			i++
 		}
 	}
-	fmt.Println(slice_[:i])  //返回下标的切片位置
+	fmt.Println(slice_[:i]) //返回下标的切片位置
 
 }
 
-func slice_1_1()  {    //TODO 去重
+func slice_1_1() { //TODO 去重
 	//练习2：写一个函数，就地消除[]string中重复字符串，如：
 	//	{"red", "black", "red", "pink", "blue", "pink", "blue"}
 	//	——>	{"red", "black", "pink", "blue"}
-	data :=[]string{"red", "black", "red", "pink", "blue", "pink", "blue"}
-	out := data[:0]//创建一个空切片
+	data := []string{"red", "black", "red", "pink", "blue", "pink", "blue"}
+	out := data[:0] //创建一个空切片
 	fmt.Println(out)
-	for _,value :=range data{//外层遍历要查询的数据
-		i :=0
-		for ;i<len(out);i++{//内层需要定义一个循环来查看自己定义的空切片是否有重复的数据，如有就跳出循环如果没有记住遍历的位置以及下标的位置添加到空切片中
-			if value == out[i]{
+	for _, value := range data { //外层遍历要查询的数据
+		i := 0
+		for ; i < len(out); i++ { //内层需要定义一个循环来查看自己定义的空切片是否有重复的数据，如有就跳出循环如果没有记住遍历的位置以及下标的位置添加到空切片中
+			if value == out[i] {
 				break
 			}
 
 		}
-		if i == len(out){
+		if i == len(out) {
 			out = append(out, value)
 		}
 	}
@@ -64,11 +64,10 @@ func slice_1_1()  {    //TODO 去重
 
 }
 
-
-func remove()  {
-	slice_1a :=[]int{4,5,6,7,8,9}
+func remove() {
+	slice_1a := []int{4, 5, 6, 7, 8, 9}
 	//   目标位置切片：下标替换元素位置     源切片：下标确认元素
-	copy(slice_1a[1:],slice_1a[3:])
+	copy(slice_1a[1:], slice_1a[3:])
 	fmt.Println(slice_1a)
 	fmt.Println(slice_1a[3:])
 }
@@ -77,13 +76,13 @@ func wcFunc(str_text string) map[string]int {
 	//练习3：	封装 wcFunc() 函数。接收一段英文字符串str。返回一个map，记录str中每个“词”出现次数的。
 	//如："I love my work and I love my family too"
 	//map[I:2 and:1 family:1 love:2 my:2 too:1 work:1]
-	list_ :=strings.Fields(str_text)
-	m := map[string] int{}
-	for i:=0;i<len(list_);i++{
-		if _,bool_vlaue :=m[list_[i]] ; bool_vlaue{
-			m[list_[i]]+=1
-		}else {
-			m[list_[i]]=1
+	list_ := strings.Fields(str_text)
+	m := map[string]int{}
+	for i := 0; i < len(list_); i++ {
+		if _, bool_vlaue := m[list_[i]]; bool_vlaue {
+			m[list_[i]] += 1
+		} else {
+			m[list_[i]] = 1
 		}
 	}
 	return m
@@ -98,14 +97,13 @@ func wcFunc(str_text string) map[string]int {
 //	fmt.Println("stop_vluae",a,b)
 //}
 
-
-func main()  {
+func main() {
 	//slice_1()
 	//noSame()
 	//slice_1_1()
 	//remove()
-	str_text :="I love my work and I love my family too"
-	a :=wcFunc(str_text)
+	str_text := "I love my work and I love my family too"
+	a := wcFunc(str_text)
 	fmt.Println(a)
 
 }
--- day_02/day_02结构体使用.go
+++ day_02/day_02结构体使用.go
@@ -7,45 +7,42 @@ import (
 
 // TODO 结构体：是一种数据类型
 
-type Person struct {   //类型定义  不能给成员里面赋值的（地位等价于 int byte boll string...）
+type Person struct { //类型定义  不能给成员里面赋值的（地位等价于 int byte boll string...）
 	name string
-	age int
-	sex byte
-
+	age  int
+	sex  byte
 }
 
 type user string //自定义数据类型
 
-
-
 //TODO 普通变量定义和初始化
 //1. 顺序初始化
-var man Person =Person{"张三",20,'m'}  //全员必须初始化
+var man Person = Person{"张三", 20, 'm'} //全员必须初始化
 
 //2.指定成员初始化：
 
 //3.结构体函数
-func test1(temp Person)  {
-	fmt.Println(unsafe.Sizeof(temp))  //TODO 查看结构体大小 返回的数字
-	temp.name="王麻子"
+func test1(temp Person) {
+	fmt.Println(unsafe.Sizeof(temp)) //TODO 查看结构体大小 返回的数字
+	temp.name = "王麻子"
 	fmt.Println(temp)
 }
-func main_01()  {
-	man2 :=Person{name:"李四"}
+func main_01() {
+	man2 := Person{name: "李四"}
 	fmt.Println(man)
 	fmt.Println(man2)
-	fmt.Printf("%q\n",man.name)   //索引成员变量
+	fmt.Printf("%q\n", man.name) //索引成员变量
 	var man3 Person
-	man3.name ="王五"
-	man3.age=20
-	man3.sex='f'
-	fmt.Printf("%q\n",man3)
+	man3.name = "王五"
+	man3.age = 20
+	man3.sex = 'f'
+	fmt.Printf("%q\n", man3)
 	// TODO 机构体比较  只能用 == ！=     不能用<>
-	var p1 Person =Person{"张三",20,'m'}
-	var p2 Person =Person{"张三",20,'m'}
-	var p3 Person =Person{"张三",201,'m'}
-	fmt.Println("p1 == p2",p1 == p2)
-	fmt.Println("p1 ==p3",p1 ==p3)
+	var p1 Person = Person{"张三", 20, 'm'}
+	var p2 Person = Person{"张三", 20, 'm'}
+	var p3 Person = Person{"张三", 201, 'm'}
+	fmt.Println("p1 == p2", p1 == p2)
+	fmt.Println("p1 ==p3", p1 == p3)
 
 	//	TODO 相同类型结构体赋值（变量类型，个数，顺序一致） 不相同的不可以
 	// TODO 结构体变量的地址 == 结构体首个元素的地址
@@ -61,72 +58,71 @@ func main_01()  {
 //TODO 指针结构体定义和使用
 type Person1 struct {
 	name string
-	age int
-	sex byte
+	age  int
+	sex  byte
 }
 
 func test3(data *Person1) {
-	data.name="高"
-	data.age=50
-	data.sex='m'
+	data.name = "高"
+	data.age = 50
+	data.sex = 'm'
 
 }
-func main3()  {
-//	TODO 1.第一种方法
-	var p1 *Person1 = &Person1{name:"王",age:20,sex:'f'}
+func main3() {
+	//	TODO 1.第一种方法
+	var p1 *Person1 = &Person1{name: "王", age: 20, sex: 'f'}
 
 	fmt.Println(*p1)
-	fmt.Printf("p1 = %T\n" ,p1)
+	fmt.Printf("p1 = %T\n", p1)
 	//TODO 第二种方法
 
-	p2 :=new(Person1)
-	p2.name="李"
-	p2.sex='m'
-	p2.age=50
-	fmt.Println("new",*p2)
+	p2 := new(Person1)
+	p2.name = "李"
+	p2.sex = 'm'
+	p2.age = 50
+	fmt.Println("new", *p2)
 
 	//TODO 结构体函数传参
 
-	fmt.Printf("%T\n",p2)
+	fmt.Printf("%T\n", p2)
 	test3(p2)
 	fmt.Println(p2)
 
-
-
 }
+
 //TODO 结构体指针做函数的返回值
 
 type Person4 struct {
 	name string
-	age int
-	sex byte
-	ids []string
+	age  int
+	sex  byte
+	ids  []string
 }
-func init_print(data *Person4)  {
-	data.name="1231"
-	data.sex='n'
-	data.age=20
-	data.ids=append(data.ids,"123","456")
+
+func init_print(data *Person4) {
+	data.name = "1231"
+	data.sex = 'n'
+	data.age = 20
+	data.ids = append(data.ids, "123", "456")
 
 }
-func init_print2()  *Person4{
+func init_print2() *Person4 {
 	//var data Person4
 	data := new(Person4)
-	data.name="666"
-	data.sex='n'
-	data.age=15
-	data.ids=append(data.ids,"123","456")
+	data.name = "666"
+	data.sex = 'n'
+	data.age = 15
+	data.ids = append(data.ids, "123", "456")
 	//TODO 返回局部变量的值， 而&data是返回局部变量的地址值的
 	return data
 }
 
 func main() {
-	var p1 Person4     //TODO 初始值函数内部传普通结构体地址的方法
+	var p1 Person4 //TODO 初始值函数内部传普通结构体地址的方法
 	init_print(&p1)
 	fmt.Println(p1)
-	p2:=init_print2() //TODO 初始值带返回值的方法 注意：不能返回局部变量的地址值，原因： 局部变量保存在栈针上，函数调用结束后，会立即释放，但是不会把值清除掉，但不会受系统保护，随时可能分配到其他程序
+	p2 := init_print2() //TODO 初始值带返回值的方法 注意：不能返回局部变量的地址值，原因： 局部变量保存在栈针上，函数调用结束后，会立即释放，但是不会把值清除掉，但不会受系统保护，随时可能分配到其他程序
 	fmt.Println(*p2)
-	fmt.Printf("%T\n",p2)
-
+	fmt.Printf("%T\n", p2)
 
 }
