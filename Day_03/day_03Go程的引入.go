74765e27024d2c2d4234298f01d28a2a69ab0016
Zxn <zxnyn0423@163.com>
2020-03-24 00:45:46 +0800

    协程的引入

---
+++ Day_03/day_03Go程的引入.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sing() {
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("-------正在唱歌----------")
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+}
+
+func dance() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("-------正在跳舞----------")
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
+func main() {
+	go sing()
+	go dance() //TODO 子任务
+	for {      //TODO 父进程
+
+	}
+}
