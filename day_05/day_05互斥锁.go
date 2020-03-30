d3e8bbe9e3a32c06e81b2aa8b757345a1245792c
Zxn <zxnyn0423@163.com>
2020-03-30 16:45:03 +0800

    互斥锁-sync.Mutex

--- day_05/day_05互斥锁.go
+++ day_05/day_05互斥锁.go
@@ -2,25 +2,54 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-var ch = make(chan int)
+//var ch = make(chan int)
+//
+//func printer(str string) {
+//	for _, ch := range str {
+//		fmt.Printf("%c", ch)
+//		time.Sleep(time.Millisecond * 1000)
+//	}
+//}
+//func person1() {
+//	printer("hello")
+//	ch <- 789
+//}
+//
+//func person2() {
+//	<-ch
+//	printer("world")
+//
+//}
+//
+//func main() {
+//	go person1()
+//	go person2()
+//	for {
+//		;
+//	}
+//}
+
+//TODO  使用锁完成同步--互斥锁
+var mutex sync.Mutex // TODO创建一个互斥量,新建的互斥锁状态为0未加锁，锁只有一把
 
 func printer(str string) {
+	mutex.Lock() // TODO 访问共享数据之前，加锁
 	for _, ch := range str {
 		fmt.Printf("%c", ch)
-		time.Sleep(time.Millisecond * 1000)
+		time.Sleep(time.Millisecond * 100)
 	}
+	mutex.Unlock() //TODO 共享数据访问结束，解锁
 }
 func person1() {
-	printer("hello")
-	ch <- 789
+	printer("hello") // 先
 }
 
 func person2() {
-	<-ch
-	printer("world")
+	printer("world") //后
 
 }
 
