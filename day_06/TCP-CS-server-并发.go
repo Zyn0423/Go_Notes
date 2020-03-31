2b3bdef08f24711b780c3eb1ddc97c19b30fb17d
Zxn <zxnyn0423@163.com>
2020-03-31 22:07:44 +0800

    socket TCP 服务端-并发版

---
+++ day_06/TCP-CS-server-并发.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"runtime"
+	"strings"
+)
+
+func HandlerConnect(conn net.Conn) {
+	defer conn.Close()
+	addr := conn.RemoteAddr() // TODO 客户端的IP地址
+	fmt.Println(addr, "服务器阻塞等待客服端连接")
+	buf := make([]byte, 1024*4) //TODO 循环客服端数据
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn.Read", err)
+			//goto Block
+			//return
+			runtime.Goexit()
+		}
+		fmt.Println("服务端接收的数据", string(buf[:n]))
+
+		conn.Write([]byte(strings.ToUpper(string(buf[:n])))) //回传大写数据
+
+	}
+	//Block:
+}
+func main() {
+	Listener, err := net.Listen("tcp", "127.0.0.1:8001")
+	if err != nil {
+		fmt.Println("net.Listen 失败 ", err)
+	}
+	defer Listener.Close()
+	for {
+		conn, err := Listener.Accept()
+		if err != nil {
+			fmt.Println("Listener.Accept 失败", err)
+			return
+		}
+		go HandlerConnect(conn)
+
+	}
+
+}
