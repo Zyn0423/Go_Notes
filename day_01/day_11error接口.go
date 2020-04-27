09569e2cdcf2b42b6c33f7f5096bb07523e15cdc
Zxn <zxnyn0423@163.com>
2020-04-27 14:20:41 +0800

    panic 函数的调用

--- day_01/day_11error接口.go
+++ day_01/day_11error接口.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func test(a, b int) (n int, err error) {
+	err = nil
 	if b == 0 {
-		fmt.Println(err)
+
 	} else {
 		n = a / b
 	}
@@ -13,7 +14,7 @@ func test(a, b int) (n int, err error) {
 func main() {
 	n, err := test(10, 0)
 	if err != nil {
-		fmt.Println("....")
+		fmt.Println("....", err)
 	}
 	fmt.Println(n)
 }
---
+++ day_01/day_12panic函数.go
@@ -0,0 +1,9 @@
+package main
+
+func test02(a, b int) int {
+	return a + b
+}
+func main() {
+	panic("系统错误")
+	test02(1, 2)
+}
